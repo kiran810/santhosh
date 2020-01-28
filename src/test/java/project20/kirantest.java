18cbdf781c10abdfd20ffb9ebb554c0c2e705a0a
kiran <kiraneng.kiran@gmail.com>
2020-01-28 21:19:04 +0530

    third

---
+++ src/test/java/project20/kirantest.java
@@ -0,0 +1,19 @@
+package project20;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class kirantest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
